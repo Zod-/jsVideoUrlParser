c2e6155b7a83cb3e9dab82e126df1f49772f754b
Julian Hangstörfer <jhangstoerfer@gmail.com>
2014-12-30 08:16:40 +0100

    Update urlParser.js

--- release/urlParser.js
+++ release/urlParser.js
@@ -38,32 +38,28 @@ URLParser.prototype.create = function(op) {
   return undefined;
 };
 
-window.urlParser = new URLParser();
+var urlParser = new URLParser();
 
 //parses strings like 1h30m20s to seconds
-function getTime(timestring) {
+function getTime(timeString) {
   "use strict";
   var totalSeconds = 0,
-  timeValues = {
-    's': 1,
-    'm': 1 * 60,
-    'h': 1 * 60 * 60,
-    'd': 1 * 60 * 60 * 24,
-    'w': 1 * 60 * 60 * 24 * 7
-  },
-  timePairs;
-
+    timeValues = {
+      's': 1,
+      'm': 1 * 60,
+      'h': 1 * 60 * 60,
+      'd': 1 * 60 * 60 * 24,
+      'w': 1 * 60 * 60 * 24 * 7
+    },
+    timePairs;
   //is the format 1h30m20s etc
-  if (!timestring.match(/^(\d+[smhdw]?)+$/)) {
+  if (!timeString.match(/^(\d+[smhdw]?)+$/)) {
     return 0;
   }
   //expand to "1 h 30 m 20 s" and split
-  timestring = timestring.replace(/([smhdw])/g, ' $1 ').trim();
-  timePairs = timestring.split(' ');
-
+  timeString = timeString.replace(/([smhdw])/g, ' $1 ').trim();
+  timePairs = timeString.split(' ');
   for (var i = 0; i < timePairs.length; i += 2) {
-    console.log(timePairs[i]);
-    console.log(timePairs[i+1]);
     totalSeconds += parseInt(timePairs[i], 10) * timeValues[timePairs[i + 1] || 's'];
   }
   return totalSeconds;
