ecd4e7d440514ebf0901778a70174ea911f5e092
Julian Hangstörfer <jhangstoerfer@gmail.com>
2016-02-21 16:53:29 +0100

    v0.1.6

--- tests/test.js
+++ tests/test.js
@@ -311,7 +311,8 @@ QUnit.test("Vimeo URLs", function (assert) {
         embed: '//player.vimeo.com/video/97688625'
       },
       urls: ['https://vimeo.com/groups/shortfilms/videos/97688625',
-        '//player.vimeo.com/video/97688625'
+        '//player.vimeo.com/video/97688625',
+        'https://vimeo.com/groups/1minute/videos/97688625'
       ]
     }, {
       videoInfo: cloneObject(vi),
@@ -322,11 +323,24 @@ QUnit.test("Vimeo URLs", function (assert) {
       urls: ['http://vimeopro.com/staff/frame/video/24069938',
         '//player.vimeo.com/video/24069938'
       ]
+    }, {
+      videoInfo: cloneObject(vi),
+      formats: {
+        long: 'https://vimeo.com/36881035#t=208',
+        embed: '//player.vimeo.com/video/36881035#t=208'
+      },
+      urls: ['https://vimeo.com/36881035#t=3m28s',
+        '//player.vimeo.com/video/36881035#t=3m28s'
+      ]
     }];
 
   tests[1].videoInfo.id = '96186586';
   tests[2].videoInfo.id = '97688625';
   tests[3].videoInfo.id = '24069938';
+  tests[4].videoInfo.id = '36881035';
+  tests[4].videoInfo.params = {
+    start: 208
+  };
   assertUrlTest(assert, tests);
 });
 
