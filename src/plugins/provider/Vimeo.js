ff9777c18c7e8015d28def8473a9b0c9e16f65cc
Julian Hangstörfer <jhangstoerfer@gmail.com>
2016-02-21 16:47:57 +0100

    Added start time for vimeo urls Closes #13

--- src/plugins/provider/Vimeo.js
+++ src/plugins/provider/Vimeo.js
@@ -1,29 +1,57 @@
 urlParser.bind({
   provider: 'vimeo',
   alternatives: ['vimeopro'],
-  parse: function (url) {
+  parse: function (url, params) {
     "use strict";
-    var match,
-      id;
-    match = url.match(/(?:\/(?:channels\/[\w]+|(?:(?:album\/\d+|groups\/[\w]+)\/)?videos?))?\/(\d+)/i);
-    id = match ? match[1] : undefined;
-    if (!id) {
+    var match;
+    var result = {
+      mediaType: 'video',
+      params: params
+    };
+
+    match = url.match(
+      /(?:\/(?:channels\/[\w]+|(?:(?:album\/\d+|groups\/[\w]+)\/)?videos?))?\/(\d+)/i
+    );
+    result.id = match ? match[1] : undefined;
+
+    if (!result.id) {
       return undefined;
     }
-    return {
-      'mediaType': 'video',
-      'id': id
-    };
+
+    if (params.hasOwnProperty('t')) {
+      params.start = getTime(params.t);
+      delete params.t;
+    }
+
+    return result;
   },
   defaultFormat: 'long',
-  formats:{
-    long: function(vi, params){
+  formats: {
+    long: function (vi, params) {
       "use strict";
-      return 'https://vimeo.com/' + vi.id + combineParams({params: params});
+      var url = 'https://vimeo.com/' + vi.id;
+      var startTime = params.start;
+      delete params.start;
+      url += combineParams({
+        params: params
+      });
+      if (startTime) {
+        url += '#t=' + startTime;
+      }
+      return url;
     },
-    embed: function(vi, params){
+    embed: function (vi, params) {
       "use strict";
-      return '//player.vimeo.com/video/' + vi.id + combineParams({params: params});
+      var url = '//player.vimeo.com/video/' + vi.id;
+      var startTime = params.start;
+      delete params.start;
+      url += combineParams({
+        params: params
+      });
+      if (startTime) {
+        url += '#t=' + startTime;
+      }
+      return url;
     }
   }
 });
