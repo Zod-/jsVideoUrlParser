c6fe4aba744d691052e90ec6ec030872f2394b3d
Julian Hangstörfer <jhangstoerfer@gmail.com>
2016-07-19 01:13:50 +0200

    Removed unnecessary param and added another url

--- tests/src/plugins/provider/CanalPlus.js
+++ tests/src/plugins/provider/CanalPlus.js
@@ -3,18 +3,17 @@ QUnit.test('CanalPlus Urls', function (assert) {
   var vi = {
       provider: 'canalplus',
       id: '1365175',
-      mediaType: 'video',
-      params: {
-        param: 'cplus'
-      }
+      mediaType: 'video'
     },
     tests = [{
       videoInfo: cloneObject(vi),
       formats: {
-        embed: 'http://player.canalplus.fr/embed/?param=cplus&vid=1365175'
+        embed: 'http://player.canalplus.fr/embed/?vid=1365175'
       },
-      urls: ['http://player.canalplus.fr/embed/?param=cplus&vid=1365175']
+      urls: [
+        'http://player.canalplus.fr/embed/?vid=1365175',
+        'http://www.canalplus.fr/humour/pid1784-les-guignols.html?vid=1365175'
+      ]
     }];
-
   assertUrlTest(assert, tests);
 });
