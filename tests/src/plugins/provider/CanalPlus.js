bab9012b853e7bb1f4d0ae9c3df45b616e86dac7
Julian Hangstörfer <jhangstoerfer@gmail.com>
2016-07-19 01:16:07 +0200

    Seperate vars

--- tests/src/plugins/provider/CanalPlus.js
+++ tests/src/plugins/provider/CanalPlus.js
@@ -1,19 +1,19 @@
 QUnit.test('CanalPlus Urls', function (assert) {
   'use strict';
   var vi = {
-      provider: 'canalplus',
-      id: '1365175',
-      mediaType: 'video'
+    provider: 'canalplus',
+    id: '1365175',
+    mediaType: 'video'
+  };
+  var tests = [{
+    videoInfo: cloneObject(vi),
+    formats: {
+      embed: 'http://player.canalplus.fr/embed/?vid=1365175'
     },
-    tests = [{
-      videoInfo: cloneObject(vi),
-      formats: {
-        embed: 'http://player.canalplus.fr/embed/?vid=1365175'
-      },
-      urls: [
-        'http://player.canalplus.fr/embed/?vid=1365175',
-        'http://www.canalplus.fr/humour/pid1784-les-guignols.html?vid=1365175'
-      ]
-    }];
+    urls: [
+      'http://player.canalplus.fr/embed/?vid=1365175',
+      'http://www.canalplus.fr/humour/pid1784-les-guignols.html?vid=1365175'
+    ]
+  }];
   assertUrlTest(assert, tests);
 });
--- tests/src/plugins/provider/Coub.js
+++ tests/src/plugins/provider/Coub.js
@@ -1,21 +1,21 @@
 QUnit.test('Coub Urls', function (assert) {
   'use strict';
   var vi = {
-      'provider': 'coub',
-      'id': 'by7sm',
-      'mediaType': 'video'
+    'provider': 'coub',
+    'id': 'by7sm',
+    'mediaType': 'video'
+  };
+  var tests = [{
+    videoInfo: cloneObject(vi),
+    formats: {
+      embed: '//coub.com/embed/by7sm',
+      long: 'https://coub.com/view/by7sm',
     },
-    tests = [{
-      videoInfo: cloneObject(vi),
-      formats: {
-        embed: '//coub.com/embed/by7sm',
-        long: 'https://coub.com/view/by7sm',
-      },
-      urls: [
-        '//coub.com/embed/by7sm',
-        'https://coub.com/view/by7sm'
-      ]
-    }];
+    urls: [
+      '//coub.com/embed/by7sm',
+      'https://coub.com/view/by7sm'
+    ]
+  }];
 
   assertUrlTest(assert, tests);
 });
