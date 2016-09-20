b4149645d5b214ad41cbdb3cfed6fdcda5b8e49e
Julian Hangstörfer <jhangstoerfer@gmail.com>
2016-09-20 13:04:19 +0200

    Don't use local urlParser(empty) for tests

--- tests/src/assertUrlTest.js
+++ tests/src/assertUrlTest.js
@@ -3,11 +3,11 @@ function assertUrlTest(assert, tests) {
   /*jshint unused:true */
   tests.forEach(function (test) {
     test.urls.forEach(function (url) {
-      assert.deepEqual(urlParser.parse(url), test.videoInfo, url);
+      assert.deepEqual(window.urlParser.parse(url), test.videoInfo, url);
     });
     for (var format in test.formats) {
       if (test.formats.hasOwnProperty(format)) {
-        assert.equal(urlParser.create({
+        assert.equal(window.urlParser.create({
           videoInfo: test.videoInfo,
           format: format,
           params: test.videoInfo.params
