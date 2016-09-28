b0826d495611b755fc5e65b73de8d2f94b6944e4
Julian Hangstörfer <jhangstoerfer@gmail.com>
2016-09-28 03:08:35 +0200

    Don't use window as argument Ref #29

--- src/footer.js
+++ src/footer.js
@@ -1,7 +1,7 @@
-if (typeof w !== 'undefined') {
-  w.urlParser = urlParser;
+if (typeof window !== 'undefined') {
+  window.urlParser = urlParser;
 }
 if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
   module.exports = urlParser;
 }
-})(window);
+})();
--- src/header.js
+++ src/header.js
@@ -1,2 +1,2 @@
-(function (w) {
+(function () {
   'use strict';
