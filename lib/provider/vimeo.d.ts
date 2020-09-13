05858be45aabaa730035c40d482713e2b575b6ba
Julian Hangstörfer <jhangstoerfer@gmail.com>
2020-09-13 18:17:02 +0200

    Revert "Add vimeo image format"

--- lib/provider/vimeo.d.ts
+++ lib/provider/vimeo.d.ts
@@ -3,16 +3,12 @@ import { VideoInfo } from '../urlParser';
 export interface VimeoUrlParameters {
     [key: string]: any;
     start?: number;
-    imageWidth?: number;
-    imageHeight?: number;
-    imageExtension?: string;
 }
 
 export type VimeoMediaTypes = 'video';
 
 export interface VimeoVideoInfo extends VideoInfo<VimeoUrlParameters, VimeoMediaTypes> {
     provider: 'vimeo';
-    channel?: string;
 }
 
 export type VimeoParseResult = VimeoVideoInfo | undefined;
