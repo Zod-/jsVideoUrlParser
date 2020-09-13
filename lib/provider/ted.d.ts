1454351e6e60f810ad28ff5ebf91069d0f6d12d8
Julian Hangstörfer <jhangstoerfer@gmail.com>
2020-09-13 17:52:45 +0200

    Add missing list

--- lib/provider/ted.d.ts
+++ lib/provider/ted.d.ts
@@ -8,6 +8,7 @@ export type TedMediaTypes = 'video' | 'playlist';
 
 export interface TedVideoInfo extends VideoInfo<TedUrlParameters, TedMediaTypes> {
     provider: 'ted';
+    list?: string;
 }
 
 export type TedParseResult = TedVideoInfo | undefined;
