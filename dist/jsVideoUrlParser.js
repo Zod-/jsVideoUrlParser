e1f2fb47cf097f9b9a5aa22303b3571950caf37b
Julian Hangstörfer <jhangstoerfer@gmail.com>
2020-11-16 01:18:35 +0100

    v0.4.3

--- dist/jsVideoUrlParser.js
+++ dist/jsVideoUrlParser.js
@@ -1288,6 +1288,98 @@
 
   base.bind(new TikTok());
 
+  var combineParams$c = util.combineParams;
+
+  function Ted() {
+    this.provider = 'ted';
+    this.formats = {
+      "long": this.createLongUrl,
+      embed: this.createEmbedUrl
+    };
+    this.mediaTypes = {
+      VIDEO: 'video',
+      PLAYLIST: 'playlist'
+    };
+  }
+
+  Ted.prototype.parseUrl = function (url, result) {
+    var match = url.match(/\/(talks|playlists\/(\d+))\/([\w-]+)/);
+    var channel = match ? match[1] : undefined;
+
+    if (!channel) {
+      return result;
+    }
+
+    result.channel = channel.split('/')[0];
+    result.id = match[3];
+
+    if (result.channel === 'playlists') {
+      result.list = match[2];
+    }
+
+    return result;
+  };
+
+  Ted.prototype.parseMediaType = function (result) {
+    if (result.id && result.channel === 'playlists') {
+      delete result.channel;
+      result.mediaType = this.mediaTypes.PLAYLIST;
+    }
+
+    if (result.id && result.channel === 'talks') {
+      delete result.channel;
+      result.mediaType = this.mediaTypes.VIDEO;
+    }
+
+    return result;
+  };
+
+  Ted.prototype.parse = function (url, params) {
+    var result = {
+      params: params
+    };
+    result = this.parseUrl(url, result);
+    result = this.parseMediaType(result);
+
+    if (!result.id) {
+      return undefined;
+    }
+
+    return result;
+  };
+
+  Ted.prototype.createLongUrl = function (vi, params) {
+    var url = '';
+
+    if (vi.mediaType === this.mediaTypes.VIDEO && vi.id) {
+      url += 'https://ted.com/talks/' + vi.id;
+    } else if (vi.mediaType === this.mediaTypes.PLAYLIST && vi.id) {
+      url += 'https://ted.com/playlists/' + vi.list + '/' + vi.id;
+    } else {
+      return undefined;
+    }
+
+    url += combineParams$c(params);
+    return url;
+  };
+
+  Ted.prototype.createEmbedUrl = function (vi, params) {
+    var url = 'https://embed.ted.com/';
+
+    if (vi.mediaType === this.mediaTypes.PLAYLIST && vi.id) {
+      url += 'playlists/' + vi.list + '/' + vi.id;
+    } else if (vi.mediaType === this.mediaTypes.VIDEO && vi.id) {
+      url += 'talks/' + vi.id;
+    } else {
+      return undefined;
+    }
+
+    url += combineParams$c(params);
+    return url;
+  };
+
+  base.bind(new Ted());
+
   var lib = base;
 
   return lib;
