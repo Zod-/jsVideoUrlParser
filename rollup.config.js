caaff2d6999e2e4f480be441cdf987f6ed1ab827
Julian Hangstörfer <jhangstoerfer@gmail.com>
2021-11-16 19:31:36 +0100

    Update obsolote dependencies and get rid of dtslint as it causes errors

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import babel from 'rollup-plugin-babel';
+import babel from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 
 export default [
